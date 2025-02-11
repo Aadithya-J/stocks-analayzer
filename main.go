7b44c3fd9cfc2c749434c017a11d8c1b1880bc30
Aadithya-J <jlaadithya@gmail.com>
2025-02-11 15:42:25 +0530

    Add functionality to scrape stock prices and save to CSV

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
@@ -9,12 +10,21 @@ import (
 	"github.com/gocolly/colly"
 )
 
-func main1() {
-	ticker := "ZOMATO.NS"
-	url := "https://finance.yahoo.com/quote/" + ticker
+type Stock struct {
+	company, price, change, changePercent string
+}
+
+func main() {
+	tickers := []string{
+		"MSFT", "IBM", "AAPL", "GOOGL", "AMZN",
+		"FB", "NFLX", "TSLA", "NVDA", "INTC",
+		"AMD", "QCOM", "ADBE", "PYPL", "CRM",
+		"ORCL", "V", "MA", "ACN", "CTSH",
+		"INFY", "ADP", "PAYX", "CDNS", "ZOMATO.NS",
+	}
 
 	c := colly.NewCollector()
-	var rawHTML string
+	rawHTMLs := []string{}
 
 	c.OnRequest(func(req *colly.Request) {
 		fmt.Println("Visiting", req.URL)
@@ -25,38 +35,65 @@ func main1() {
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		rawHTML = string(r.Body)
-
-		err := os.WriteFile("output.html", r.Body, 0644)
-		if err != nil {
-			log.Println("Error writing to file:", err)
-		}
+		rawHTML := string(r.Body)
+		rawHTMLs = append(rawHTMLs, rawHTML)
 	})
 
-	c.Visit(url)
+	for _, ticker := range tickers {
+		url := "https://finance.yahoo.com/quote/" + ticker
+		c.Visit(url)
+	}
 	c.Wait()
 
 	priceRegex := regexp.MustCompile(`data-testid="qsp-price">([\d.,]+)`)
 	changeRegex := regexp.MustCompile(`data-testid="qsp-price-change">(-?[\d.,]+)`)
 	changePercentRegex := regexp.MustCompile(`data-testid="qsp-price-change-percent">\(([-+]?[\d.,]+%)\)`)
 
-	priceMatch := priceRegex.FindStringSubmatch(rawHTML)
-	price := "N/A"
-	if len(priceMatch) > 1 {
-		price = priceMatch[1]
+	stocks := []Stock{}
+	for _, rawHTML := range rawHTMLs {
+		stock := Stock{}
+		priceMatch := priceRegex.FindStringSubmatch(rawHTML)
+		stock.price = "N/A"
+		if len(priceMatch) > 1 {
+			stock.price = priceMatch[1]
+		}
+
+		changeMatch := changeRegex.FindStringSubmatch(rawHTML)
+		stock.change = "N/A"
+		if len(changeMatch) > 1 {
+			stock.change = changeMatch[1]
+		}
+
+		changePercentMatch := changePercentRegex.FindStringSubmatch(rawHTML)
+		stock.changePercent = "N/A"
+		if len(changePercentMatch) > 1 {
+			stock.changePercent = changePercentMatch[1]
+		}
+
+		stocks = append(stocks, stock)
 	}
 
-	changeMatch := changeRegex.FindStringSubmatch(rawHTML)
-	change := "N/A"
-	if len(changeMatch) > 1 {
-		change = changeMatch[1]
+	for i, stock := range stocks {
+		fmt.Printf("Stock %d:\nPrice: %s\nChange: %s\nChange Percent: %s\n\n", i+1, stock.price, stock.change, stock.changePercent)
 	}
 
-	changePercentMatch := changePercentRegex.FindStringSubmatch(rawHTML)
-	changePercent := "N/A"
-	if len(changePercentMatch) > 1 {
-		changePercent = changePercentMatch[1]
+	fmt.Println(stocks)
+
+	file, err := os.Create("stocks.csv")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, stock := range stocks {
+		row := []string{stock.price, stock.change, stock.changePercent}
+		err := writer.Write(row)
+		if err != nil {
+			log.Fatal("Cannot write to file", err)
+		}
 	}
 
-	fmt.Printf("Stock: %s\nPrice: %s\nChange: %s\nChange Percent: %s\n", ticker, price, change, changePercent)
+	fmt.Println("Data has been written to stocks.csv")
 }
